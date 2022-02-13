f8bbdefa0627023d6ece97896950b11573bc6672
Doonywoony <javismcarr@gmail.com>
2022-02-12 20:20:29 -0500

    modified dashboard and home routes/ created user-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,27 +3,26 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
-  console.log(req.session);
-
   Post.findAll({
-    attributes: ["id", "title", "created_at", "post_content"],
+    attributes: ["id", "title", "post_text", "created_at"],
     include: [
       {
         model: Comment,
         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
         include: {
           model: User,
-          attributes: ["username", "twitter", "github"],
+          attributes: ["username"],
         },
       },
       {
         model: User,
-        attributes: ["username", "twitter", "github"],
+        attributes: ["username"],
       },
     ],
   })
     .then((dbPostData) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
+      // pass a single post object into the homepage template
       res.render("homepage", {
         posts,
         loggedIn: req.session.loggedIn,
@@ -40,17 +39,7 @@ router.get("/login", (req, res) => {
     res.redirect("/");
     return;
   }
-
   res.render("login");
-});
-
-router.get("/signup", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
-    return;
-  }
-
-  res.render("signup");
 });
 
 router.get("/post/:id", (req, res) => {
@@ -58,32 +47,31 @@ router.get("/post/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ["id", "title", "created_at", "post_content"],
+    attributes: ["id", "title", "post_text", "created_at"],
     include: [
       {
         model: Comment,
         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
         include: {
           model: User,
-          attributes: ["username", "twitter", "github"],
+          attributes: ["username"],
         },
       },
       {
         model: User,
-        attributes: ["username", "twitter", "github"],
+        attributes: ["username"],
       },
     ],
   })
     .then((dbPostData) => {
       if (!dbPostData) {
-        res.status(404).json({ message: "No post found with this id" });
+        res.status(404).json({ message: "No Post found with this id" });
         return;
       }
-
-      // serialize the data
+      //serialize the data
       const post = dbPostData.get({ plain: true });
 
-      // pass data to template
+      //pass data to the template
       res.render("single-post", {
         post,
         loggedIn: req.session.loggedIn,
